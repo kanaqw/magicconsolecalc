4a9b95258c24704f6a971ff64f02d22ec4307302
kanaqw <81157913+kanaqw@users.noreply.github.com>
2021-04-12 16:27:23 +0700

    Create magiccalc_v1.1.cpp

---
+++ magiccalc_v1.1.cpp
@@ -0,0 +1,70 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+enum class Operation :char {
+	Add = '+',
+	Subtract = '-',
+	Multiply = '*',
+	Divide = '/'
+
+};
+
+struct Calculator {
+	
+	
+
+	Calculator() {
+		Operation add = Operation::Add;
+		char op = 'c';
+		op: add;
+
+	}
+
+		int calculate(int a, int b, char op) {
+		int sum = a + b;
+		int div = a / b;
+		int mul = a * b;
+		int sub = a - b;
+		switch (op) {
+		case ('+'):
+
+			printf("x + y = %d\n", sum);
+			return sum;
+			break;
+		case ('/'):
+
+			printf("x / y = %d\n", div);
+			return div;
+			break;
+		case ('*'):
+
+			printf("x * y = %d\n", mul);
+			return mul;
+			break;
+		case ('-'):
+
+			printf("x - y = %d\n", div);
+			return sub;
+			break;
+		default:
+			printf("Sorry luv, didnt get it\n");
+		}
+
+
+	};
+};
+	int main() {
+		int first_var;
+		int second_var;
+		char oper;
+		printf("First var: \n");
+		std::cin >> first_var;
+		printf("Second var: \n");
+		std::cin >> second_var;
+		printf("your op: +, - , / or *: \n");
+		std::cin >> oper;
+		Calculator calc;
+		calc.calculate(first_var, second_var,oper);
+		return 0;
+	};
